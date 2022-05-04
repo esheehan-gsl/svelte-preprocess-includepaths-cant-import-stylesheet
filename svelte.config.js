97f46795da19393073df5b69d1cdaf2ec1713618
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-05-04 15:52:39 -0600

    Add US Web Design System
    - Install USWDS and set up the basic SCSS files they recommend
    - Configure svelte-preprocessor in svelte.config.js
    - Import styles.scss into index.svelte
    
    This recreates the problem I'm having in another project getting USWDS
    to compile with SvelteKit.

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: preprocess({
+    includePaths: [
+      'node_modules/@uswds/uswds/packages'
+    ]
+  }),
 	kit: {
 		adapter: adapter()
 	}
