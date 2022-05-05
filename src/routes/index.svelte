4e3fd24c89e29850b632137746f092ecfa14e3ee
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-05-05 07:45:52 -0600

    Fix the SCSS config
    The reason my includePaths were not working because importing the
    stylesheet in the <script> tag was processing it through Vite (which I
    discovered has built-in Sass support), not through svelte-preprocess.
    If I @forward my root stylesheet in a <style> tag in index.svelte, there
    are no errors.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
-<script>
-  import '../styles.scss';
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+  @forward "src/styles.scss";
+</style>
